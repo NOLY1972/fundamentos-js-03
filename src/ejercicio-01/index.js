8e3abc44d471a36fc12eb889151957e69f1f1933
Fernando <fernando30041972@hotmail.com>
2023-11-02 22:54:53 +0100

    Añadir ejercicio 2

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,6 +1,6 @@
 // Escribe tu código aquí:
- const texto_original = 'hola que tal'
- const texto_mayusculas = texto_original.toUpperCase()
+ const texto_original = 'Hola que Tal'
+ const lower_texto_minusculas = texto_original.toLowerCase()
 
- console.log(texto_mayusculas)
+ console.log(lower_texto_minusculas)
 
